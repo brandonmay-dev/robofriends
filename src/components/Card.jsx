f693d63186b862a441fee77da411b1ebff8cd49c
Marqui <bmay86@protonmail.com>
2026-01-15 09:21:23 -0500

    updated card.jsx information

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
   return (
-    <div>
-      <img alt="Robot" src="https://robohash.org/test" />
+    <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
+      <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
       <div>
-        <h2>Robot Name</h2>
-        <p>jane.doe@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
