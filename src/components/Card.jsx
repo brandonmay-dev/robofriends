0d56dc803c9074fa16be0222d8c0ab1a8c979051
Marqui <bmay86@protonmail.com>
2026-01-15 09:10:15 -0500

    add img site

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = () => {
   return (
     <div>
-      <img alt="Robot" src="" />
+      <img alt="Robot" src="https://robohash.org/test" />
       <div>
         <h2>Robot Name</h2>
         <p>jane.doe@gmail.com</p>
