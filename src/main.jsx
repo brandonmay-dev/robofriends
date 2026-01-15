23e52723f8aabfe838561209d9baa8195b22a474
Marqui <bmay86@protonmail.com>
2026-01-15 09:14:02 -0500

    add import 'tachyons'

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import Card from "./components/Card.jsx";
+import "tachyons";
+import { StrictMode } from "react";
+
 // import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
