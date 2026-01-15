7170e093d2147ce7919cd0a30bd8229651113a10
Marqui <bmay86@protonmail.com>
2026-01-15 08:58:48 -0500

    create initial card component information

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img alt="Robot" src="" />
+      <div>
+        <h2>Robot Name</h2>
+        <p>jane.doe@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
