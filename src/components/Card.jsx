22a42b596ec02e7433ae85f574ee391558343ef0
Marqui <bmay86@protonmail.com>
2026-01-18 19:34:51 -0500

    Add a fixed width and make the image fill the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-
 const Card = ({ name, email, id }) => {
   return (
-    <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
-      <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
+    <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5 w5">
+      <img
+        alt="robots"
+        src={`https://robohash.org/${id}?size=200x200`}
+        className="w-100"
+      />
       <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
+        <h2 className="f4">{name}</h2>
+        <p className="f6">{email}</p>
       </div>
     </div>
   );
