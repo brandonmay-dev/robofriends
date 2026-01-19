7b2a6310c1b881ba05802fb2fcc91d9292120179
Marqui <bmay86@protonmail.com>
2026-01-18 19:48:55 -0500

    give the cards a flex container so they align consistently

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,19 +1,11 @@
-import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
   return (
-    <div>
-      {robots.map((user, i) => {
-        return (
-          <Card
-            key={i}
-            id={robots[i].id}
-            name={robots[i].name}
-            email={robots[i].email}
-          />
-        );
-      })}
+    <div className="flex flex-wrap justify-center">
+      {robots.map((user) => (
+        <Card key={user.id} id={user.id} name={user.name} email={user.email} />
+      ))}
     </div>
   );
 };
